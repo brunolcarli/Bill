5b0e96262986dc580d4678ee2c0549d1a155f21e
Bruno L. Carli <brunolcarli@gmail.com>
2019-07-25 20:13:05 -0300

    ADD Season query

--- abp/schema.py
+++ abp/schema.py
@@ -1,11 +1,36 @@
 import graphene
+from abp.models import Season
+
+
+class SeasonType(graphene.ObjectType):
+    class Meta:
+        interfaces = (graphene.relay.Node,)
+
+    reference = graphene.String()
+    start_date = graphene.Date()
+    end_date = graphene.Date()
+    description = graphene.String()
+
+    # TODO add tournaments
+    # TODO add league
+
+
+class SeasonlConnection(graphene.relay.Connection):
+    class Meta:
+        node = SeasonType
 
 
 class Query(object):
     '''
         ABP Queries
     '''
+    node = graphene.relay.Node.Field()
 
-    hello_bill = graphene.String()
-    def resolve_hello_bill(self, info, **kwargs):
-        return 'Hello buddy'
+    ###################################################
+    #                       Seasons                   #
+    ###################################################
+    seasons = graphene.relay.ConnectionField(
+        SeasonlConnection
+    )
+    def resolve_seasons(self, info, **kwargs):
+        return Season.objects.all()
