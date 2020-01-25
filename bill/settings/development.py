6a8f459274abb942798b05c510ac677227580e6b
Bruno L. Carli <brunolcarli@gmail.com>
2020-01-24 23:18:49 -0300

    CHANGE development settings to use on repl.it

--- bill/settings/development.py
+++ bill/settings/development.py
@@ -1,3 +1,7 @@
+"""
+Configs par arodar no Repl.it
+"""
 from bill.settings.common import *
+from decouple import config
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
