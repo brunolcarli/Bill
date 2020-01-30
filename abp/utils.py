50c970e11497f621f48ad0cdd75f0ae0e5758bae
Bruno L. Carli <brunolcarli@gmail.com>
2020-01-29 21:49:13 -0300

    ADD lv updater tools

--- abp/utils.py
+++ abp/utils.py
@@ -1,6 +1,7 @@
 """
 Módulo para ferramentas utili†arias.
 """
+from math import ceil
 from graphql_relay import from_global_id
 
 
@@ -18,3 +19,27 @@ def validate_global_id(global_id, expected_type):
         raise Exception(f'The given ID is not a {expected_type} ID!')
 
     return int_value
+
+
+def next_lv(level):
+    """
+    Fórmula para avançar de nível.
+    """
+    return ceil((4 *(level ** 3)) / 5)
+
+
+def get_exp(trainer_lv, leader_lv):
+    """
+    Retorna a quantidade de exp apos uma batalha.
+    A exp depende do lv dos treinadores envolvidos.
+    """
+    return ceil((2 * leader_lv * (trainer_lv * 1.5)))
+
+
+def lv_update(trainer):
+    """
+    Atualiza o lv e experiência do treinador/lider.
+    """
+    while trainer.exp >= trainer.next_lv:
+        trainer.lv += 1
+        trainer.next_lv = next_lv(trainer.lv)
