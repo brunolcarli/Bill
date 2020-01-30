f30cc76b0c1bb9f6bc2e63ea79a7277e157a27a1
Bruno L. Carli <brunolcarli@gmail.com>
2020-01-29 23:52:55 -0300

    CHANGE fix wsgi

--- bill/wsgi.py
+++ bill/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bill.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bill.settings.common')
 
 application = get_wsgi_application()
